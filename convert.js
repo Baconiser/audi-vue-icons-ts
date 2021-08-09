63e33cc6088c3e28e5762294df469b3e2014449e
michael.lebedew <michael.lebedew@web-dienstleister.de>
2021-08-09 13:35:30 +0200

    build fixed

--- convert.js
+++ convert.js
@@ -10,20 +10,24 @@ const out = "./dist";
 function capitalize(text) {
     return [ text[0].toUpperCase(), text.substr(1, text.length) ].join("");
 }
-const ICON_PATH = "./node_modules/@audi/audi-icon/dist/svg/static";
+
+const ICON_PATH = "../@audi/audi-icon/dist/svg/static";
+
 async function loadFiles() {
     let names = [];
     try {
         const files = await fs.readdir(ICON_PATH);
         const svgs = files.filter(isSvg);
-
+        await new Promise((resolve => fs.mkdir("./dist", () => {
+            resolve();
+        })));
         for (var i = 0; i < svgs.length; i++) {
             const file = svgs[i];
             const filename = file.replace(".svg", "");
             const splittedName = filename.split("-");
             const name = splittedName.map(capitalize).join("");
             const size = splittedName.slice(-1).includes("large") ? 48 : 24;
-            let content = await fs.readFile(path.join("./svg", file), "utf-8");
+            let content = await fs.readFile(path.join(ICON_PATH, file), "utf-8");
             content = content.replace(`width="${size}"`, ":width=\"width\"");
             content = content.replace(`height="${size}"`, ":height=\"height\"");
             content = content.replace(filename, "{{title}}");
@@ -45,8 +49,11 @@ async function loadFiles() {
         }
         let index = "";
 
-        names.forEach(name => index += `import ${name} from './dist/${name}.vue';\n`);
-        index += `export { ${names.join(",\n")} }`;
+        names.forEach(name => {
+            index += `import ${name} from './dist/${name}.vue';\n`;
+        });
+        names.forEach(name => index += `export ${name};`)
+//        index += `export { ${names.join(",\n")} }`;
         await fs.writeFile("./index.d.ts", index);
     } catch (error) {
         console.error(error);
